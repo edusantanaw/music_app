82d6f22df9cec486d30ddbaea277029d95967a64
edusantanaw <eduardosantana20122002@gmail.com>
2025-02-08 20:19:36 -0300

    feat: configuração do typeorm e criação da entidade do usuario

---
+++ backend/src/main/config/typeorm.ts
@@ -0,0 +1,26 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import * as env from "dotenv";
+import { User } from "main/infra/entities/User";
+
+env.config();
+
+const HOST = process.env.DB_HOST;
+const USER = process.env.DB_USER;
+const PASSWORD = process.env.DB_PASSWORD;
+const DATABASE = process.env.DATABASE;
+const PORT = Number(process.env.DB_PORT ?? 5432);
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: HOST,
+  port: PORT,
+  username: USER,
+  password: PASSWORD,
+  database: DATABASE,
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+});
